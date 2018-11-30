33fef745710a5a2f69f5c6447ef576168c8ac7a2
AbelLim <abel.lim@my.jcu.edu.au>
2018-11-30 09:44:11 +0800

    Initial commit

---
+++ app/src/main/java/com/example/arx8l/cp3406_assignment_1/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.arx8l.cp3406_assignment_1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
